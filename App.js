9b0e8120d6982b19f5d9e614faa024c0611581cb
Jonas Vicente <jayv.jonas@gmail.com>
2021-06-07 20:18:43 -0300

    feat: componente Comments

--- App.js
+++ App.js
@@ -5,10 +5,20 @@ import Post from './src/components/Post'
 
 export default class App extends Component {
   render() {
+    const comments = [
+      {
+        nickname: 'Jãozin Gameplays',
+        comment: 'Fotão!'
+      },
+      {
+        nickname: 'Tia Neyde',
+        comment: 'Lindo, sobrinho da titia'
+      },
+    ]
     return (
       <View style={{flex: 1}}>
         <Header/>
-        <Post image={require('./assets/imgs/fence.jpg')}/>
+        <Post image={require('./assets/imgs/fence.jpg')} comments={comments}/>
       </View>
     )
   }
